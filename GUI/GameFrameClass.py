635115a554d4557c6a2614da64be9f5e2290da1b
Abdulrahman-Aladdin <abdulrahmanalaaeldin1@gmail.com>
2023-11-13 17:19:28 +0200

    refactor upperFrameClass and add interactions between agent, controller and gui

--- GUI/GameFrameClass.py
+++ GUI/GameFrameClass.py
@@ -1,15 +1,15 @@
-from customWidgets.MainFrameClass import MainFrame
 from LowerFrameClass import LowerFrame
 from UpperFrameClass import UpperFrame
 import customtkinter as ctk
 
 
 class GameFrame(ctk.CTkFrame):
-    def __init__(self, com_score, user_score, **kwargs):
+    def __init__(self, com_score, user_score, state_value, agent_fn, **kwargs):
         super().__init__(**kwargs)
 
-        self.upper_frame = UpperFrame(master=self, com_score=com_score, user_score=user_score)
+        self.upper_frame = UpperFrame(master=self, agent_fn=agent_fn)
         self.upper_frame.place(x=0, y=0, relwidth=1, relheight=.7)
 
-        self.lower_frame = LowerFrame(master=self)
+        self.lower_frame = LowerFrame(master=self, agent_score=com_score,
+                                      user_score=user_score, state_value=state_value)
         self.lower_frame.place(x=0, rely=.7, relwidth=1, relheight=.3)
--- GUI/customWidgets/MainFrameClass.py
+++
@@ -1,6 +0,0 @@
-from customtkinter import CTkFrame
-
-
-class MainFrame(CTkFrame):
-    def __init__(self, com_score, user_score, **kwargs):
-        super().__init__(**kwargs, corner_radius=0, fg_color='transparent')
