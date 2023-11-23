9e3e0238a869438a97d88c5f6abb6c52a5e4f220
Abdulrahman-Aladdin <abdulrahmanalaaeldin1@gmail.com>
2023-11-23 21:47:06 +0200

    nothing just modify font size

--- GUI/LowerFrameClass.py
+++ GUI/LowerFrameClass.py
@@ -6,11 +6,11 @@ class LowerFrame(ctk.CTkFrame):
     def __init__(self, agent_score, user_score, state_value, **kwargs):
         super().__init__(**kwargs)
         agent_score_label = ctk.CTkLabel(master=self, textvariable=agent_score,
-                                         font=ctk.CTkFont(size=SMALL_FONT_SIZE, family=FONT_FAMILY))
+                                         font=ctk.CTkFont(size=MEDIUM_FONT_SIZE, family=FONT_FAMILY))
         user_score_label = ctk.CTkLabel(master=self, textvariable=user_score,
-                                        font=ctk.CTkFont(size=SMALL_FONT_SIZE, family=FONT_FAMILY))
+                                        font=ctk.CTkFont(size=MEDIUM_FONT_SIZE, family=FONT_FAMILY))
         state_value_label = ctk.CTkLabel(master=self, textvariable=state_value,
-                                         font=ctk.CTkFont(size=SMALL_FONT_SIZE, family=FONT_FAMILY))
+                                         font=ctk.CTkFont(size=MEDIUM_FONT_SIZE, family=FONT_FAMILY))
         agent_score_label.pack()
         user_score_label.pack()
         state_value_label.pack()
